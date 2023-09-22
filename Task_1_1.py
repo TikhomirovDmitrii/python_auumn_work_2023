2f3636e30f913c806a9110e5c20846361a0aa17b
Dmitrii <mamashaarb@gmail.com>
2023-09-22 12:48:40 +0300

    Task for lesson #2 all 3 tasks

--- Task_1_1.py
+++ Task_1_1.py
@@ -1,24 +1,3 @@
-
-x = 13
-y = 25
-xy = x * y
-print(f"{xy}")
-
-a = 5
-b = 9
-ab = a + b
-print(f'{ab}')
-
-x = int(input())
-y = int(input())
-max_value = max(x + y, x - y, x * y, x / y, x // y)
-print(max_value)
-
-
-x = int(input())
-y = int(input())
-max_value = (x + y, x - y, x * y, x / y, x // y)
-res_list = set(max_value)
-res_list.remove(max(max_value))
-print(max(res_list))
-
+n = int(input())
+for i in range(1, n * 1):
+    print ('+'*i)
